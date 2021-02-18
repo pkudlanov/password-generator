5385db7a40b2fd0c7c8032ade58a4ac56d2aeda9
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-17 19:02:06 -0500

    Turned the application to a single page app all the functionality is on one page generating a password doesn't take you to a new page

--- generator/views.py
+++ generator/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
 import random
 
-# Create your views here.
-
 
 def home(request):
-    return render(request, 'generator/home.html')
-
-
-def about(request):
-    return render(request, 'generator/about.html')
-
-
-def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
     if request.GET.get('uppercase'):
         characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        upper = 'checked'
+    else:
+        upper = ''
     if request.GET.get('special'):
         characters.extend('!@#$%^&*?()-+=|~')
+        special = 'checked'
+    else:
+        special = ''
     if request.GET.get('numbers'):
         characters.extend('0123456789')
+        numbers = 'checked'
+    else:
+        numbers = ''
 
     length = int(request.GET.get('length', 12))
 
@@ -28,4 +27,14 @@ def password(request):
     for x in range(length):
         thepassword += random.choice(characters)
 
-    return render(request, 'generator/password.html', {'password': thepassword})
+    return render(request, 'generator/home.html', {
+        'password': thepassword,
+        'length': length,
+        'upper': upper,
+        'special': special,
+        'numbers': numbers,
+    })
+
+
+def about(request):
+    return render(request, 'generator/about.html')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -18,6 +18,5 @@ from generator import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('password/', views.password, name='password'),
     path('about/', views.about, name='about'),
 ]
